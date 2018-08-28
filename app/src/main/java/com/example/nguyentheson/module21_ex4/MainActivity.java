527abe9206156b8747f23cab61fc11f98d62f947
Nguyen The Son <ngthe.son98883@gmail.com>
2018-08-28 17:37:47 +0700

    update 28-08

--- app/src/main/java/com/example/nguyentheson/module21_ex4/MainActivity.java
+++ app/src/main/java/com/example/nguyentheson/module21_ex4/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.nguyentheson.module21_ex4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtName,txtCMND;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSubmit = (Button) findViewById(R.id.btn_send);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkName()){
+                    Toast.makeText(MainActivity.this, "Không hợp lệ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+    }
+    public boolean checkName(){
+        txtName = (EditText) findViewById(R.id.text_name);
+        int length = txtName.length();
+        if(length<3) return false;
+        return true;
     }
 }
